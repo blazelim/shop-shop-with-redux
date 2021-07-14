58364cf5d21fd8421e416f9ffa4e32139a3f0851
Blaze Lim <blazebentleycolim@gmail.com>
2021-07-13 19:22:07 -0700

    testing the cart

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,15 @@
 import React from "react";
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
-import Cart from "../components/Cart";
+import ProductList from "../components/ProductList/index.js";
+import CategoryMenu from "../components/CategoryMenu/index.js";
+import Cart from "../components/Cart/index.js";
 
 const Home = () => {
 
   return (
     <div className="container">
-      {/* <CategoryMenu  /> */}
-      <ProductList  />
-      {/* <Cart /> */}
+      {/* <CategoryMenu  />
+      <ProductList  /> */}
+      <Cart />
     </div>
   );
 };
