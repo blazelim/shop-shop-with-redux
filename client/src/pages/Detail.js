4cb5ae8bab469616244ad5119a9d1fb75b0f41a0
Blaze Lim <blazebentleycolim@gmail.com>
2021-07-18 02:20:53 -0700

    removed code for context api

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -4,7 +4,6 @@ import { useQuery } from '@apollo/client';
 
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
-import { useStoreContext } from '../utils/GlobalState'
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY, ADD_TO_CART, UPDATE_PRODUCTS } from "../utils/actions"
 import { idbPromise } from '../utils/helpers';
 
